42ed88035529ca88e3fe5d9db04f0f888f702248
tibbydude <micgou@outlook.com>
2021-10-15 23:18:29 -0700

    Update Province.cs

--- Models/Province.cs
+++ Models/Province.cs
@@ -5,6 +5,8 @@ namespace lab04.Models{
     public class Province{
         [Key]
         public string ProvinceCode { get; set; }
+        
+        [Display (Name = "Province")]
         public string ProvinceName { get; set; }
 
         public List<City> Cities { get; set; }
