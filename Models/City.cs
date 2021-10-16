21d59f3a7128e5c099f3b07ae91dc25bab99e747
tibbydude <micgou@outlook.com>
2021-10-15 23:17:58 -0700

    Update City.cs

--- Models/City.cs
+++ Models/City.cs
@@ -4,9 +4,12 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace lab04.Models{
     public class City{
         public int CityId { get; set; }
+        
+        [Display (Name = "City Name")]
         public string CityName { get; set; }
         public int Population { get; set; }
 
+        [Display (Name = "Province")]
         public string ProvinceCode { get; set; }
         
         [ForeignKey("ProvinceCode")]
